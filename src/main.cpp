bef72302d5e3954e6bc0566eb6a373a29df9ff38
minhnv <nguyenminh_asp@yahoo.com.vn>
2015-04-27 00:14:00 +0700

    fix comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : main.cpp
-// Author      : MinhNv
+// Author      : MinhNv16
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
